602353d8534e38980cdd002c475da2630eb13eef
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2013-06-24 15:50:17 +0200

    JS Nav Books

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -3,7 +3,19 @@
 
 $(function() {
 
-  // Agreement condition lightbox
+  // Navbar Jeux
+  $('.NavBarLi').hide();
+  $('.NavBarLink').on('click', function(event) {
+    event.preventDefault();
+    $('.NavBarLi').fadeIn('fast');
+  });
+
+  $('.NavBarLink2').on('click', function(event) {
+    event.preventDefault();
+    $('.NavBarLi').fadeOut('fast');
+  });
+
+  // LightBox
   $('#lightbox').hide();
 
   $('.lightbox_link').on('click', function(event) {
@@ -18,17 +30,34 @@ $(function() {
     $('#lightbox').slideUp('fast');
   });
 
-  $('.NavBarLi').hide();
+  // Nav Position Books
+  var position;
 
-  $('.NavBarLink').on('click', function(event) {
-    event.preventDefault();
+  $('.books').hover(function() {
+    position = $(this).position();
 
-    $('.NavBarLi').fadeIn('fast');
-  });
+    var active_item = $(this);
+    var active_position_left = $(this).position();
+    var item_left = active_position_left.left - 40;
 
-  $('.NavBarLink2').on('click', function(event) {
-    event.preventDefault();
+    active_item.addClass('active').css({
+      'z-index': '2000',
+      'top': '15px',
+      'left': item_left,
+      'max-width': '185px',
+      'max-height': '280px'
+    });
+  },
 
-    $('.NavBarLi').fadeOut('fast');
+  function() {
+    var active_item = $(this);
+
+    active_item.removeClass('active').css({
+      'z-index': '1000',
+      'top': position.top,
+      'left': position.left,
+      'max-width': '145px',
+      'max-height': '266px'
+    });
   });
 });
