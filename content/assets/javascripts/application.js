e1982a5c0d72694b0e2dc6362bce8dde5f6c4806
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2013-06-27 15:32:15 +0200

    Lightbox

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,7 +1,23 @@
 // =require modernizr
 // =require jquery
+// =require underscore
 
-$(function() {
+$(function($, undefined) {
+
+  scrollbarWidth = (function() {
+    var $inner = jQuery('<div style="width: 100%; height:200px;">test</div>'),
+    $outer = jQuery('<div style="width:200px;height:150px; position: absolute; top: 0; left: 0; visibility: hidden; overflow:hidden;"></div>').append($inner),
+    inner = $inner[0],
+    outer = $outer[0];
+
+    jQuery('body').append(outer);
+    var width1 = inner.offsetWidth;
+    $outer.css('overflow', 'scroll');
+    var width2 = outer.clientWidth;
+    $outer.remove();
+
+    return (width1 - width2);
+  })();
 
   // Navbar Jeux
   $('.NavBarLi').hide();
@@ -14,6 +30,76 @@ $(function() {
     event.preventDefault();
     $('.NavBarLi').fadeOut('fast');
   });
+
+  // Overlay
+  var overlayToggle = window.overlayToggle = function(el, options) {
+    options = _.defaults(options || {}, {
+      loader: true
+    });
+
+    el = $(el);
+    if (el.has('.overlay').length === 0) {
+      el.css('position', 'relative');
+      var overlay = $('<div class="overlay"><div class="background" /></div>');
+      if (options.loader) {
+        overlay.append('<div class="loader" />');
+      }
+      overlay.appendTo(el).fadeIn();
+    } else {
+      el.children('.overlay').fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  }
+
+  // LightBox Show
+  var lightboxShow = function() {
+    console.log('click');
+    var item_class = $(this).first();
+    scrollPosition = $(window).scrollTop();
+
+    $('#details article').show();
+    $('section#main > *').wrapAll('<div />').parent()
+    .css({
+      position: 'fixed',
+      top: '0',
+      'margin-top': -scrollPosition + 'px',
+      width: $('section#main').width()
+    });
+    $('section#main').css('padding-right', scrollbarWidth + 'px');
+    $(window).scrollTop(0);
+
+    $('body').css({
+      height: Math.max($(window).outerHeight() - ($('#details').outerHeight(true) - $('#details').outerHeight()), $('#details').outerHeight()),
+      'padding-bottom': $(window).outerHeight() <= $('#details').outerHeight() ? '50px' : '0px'
+    });
+
+    overlayToggle($('body'), { loader: false });
+    $('body > .overlay').css({
+      height: $('section#main > div').outerHeight(),
+    });
+
+    $('#details').fadeIn();
+
+    // var item = _.find(items, function(item, key) { return item.index === parseInt(item_class.match(/\d+/)[0]) });
+    // History.setLocation({item: item.key });
+    // _gaq.push(['_trackPageview', '/items/' + item.key]);
+  }
+
+  // LightBox Hide
+  var hideDetail = function(){
+    overlayToggle($('body'));
+
+    $('#details').fadeOut(function() {
+      $('section#main').css('padding-right', 0);
+      $('header, section.content, footer').unwrap();
+      $(window).scrollTop(scrollPosition);
+      $('#details article').hide();
+    });
+
+    History.resetLocation();
+    _gaq.push(['_trackPageview', '/']);
+  }
 
   // Nav Position Books
   var position;
@@ -45,4 +131,16 @@ $(function() {
       'max-height': '266px'
     });
   });
-});
+
+  // init
+  $(function() {
+    // LightBox Initialisation
+    $('#item_lightbox').on('click', lightboxShow);
+    $('#details .close').on('click', hideDetail);
+  });
+})(jQuery);
+
+
+
+
+
